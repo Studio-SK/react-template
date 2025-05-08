4f54edbe34ebcb2523031dec7fab443fca633a2f
sarthak kumar <sarthakkumar10151@gmail.com>
2025-05-08 21:04:40 +0530

    eslint config

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,3 +1,3 @@
 export const App = () => {
-    return <h1 className="text-teal-700 text-2xl text-center">Hello world</h1>
-}
+    return <h1 className="text-teal-700 text-2xl text-center">Hello world</h1>;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './App/App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App/App";
 
-import './main.css'
+import "./main.css";
 
+// eslint-disable-next-line prettier/prettier
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
